9cfcb54601054f5e729cadd38fb7527864a0289a
kennithigot <kennithigot1@gmail.com>
2024-09-16 16:33:57 +0800

    DAV update

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Unique Cable Network, INC.</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="style.css" rel="stylesheet">
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    </head>
+    <body>
+
+        <nav class="navbar navbar-expand-lg pb-4" style="background-color:#0a2cb1;">
+            <div class="container">
+                <div class="d-flex justify-content-center align-items-center">
+                <img src="src/imgs/logo.png" class="navbar-bran filtered-img"></img>
+                <h6 class="text-light px-1 logo-info">&nbspUnique Cable <br>&nbspInternet Services</h6>
+                </div>
+                
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 "></ul>
+                    <div class="d-flex px-2 fs-5 fw-bold hov-link" role="search">
+                        <li class="nav-item navbar-nav btn-link text-decoration-none">
+                            <a class="btn p-2 px-3" aria-current="page" href="#">Home</a>
+                        </li>
+                    </div>
+                    <div class="d-flex px-2 fs-5 fw-bold hov-link" role="search">
+                        <li class="nav-item navbar-nav btn-link text-decoration-none">
+                            <a class="btn p-2 px-3" aria-current="page" href="#">About Us</a>
+                        </li>
+                    </div>
+                    <div class="d-flex px-2 fs-5 fw-bold hov-link" role="search">
+                        <li class="nav-item navbar-nav btn-link text-decoration-none">
+                            <a class="btn p-2 px-3" aria-current="page" href="#">Work</a>
+                        </li>
+                    </div>
+                    <div class="d-flex px-2 fs-5 fw-bold hov-link" role="search">
+                        <li class="nav-item navbar-nav btn-link text-decoration-none">
+                            <a class="btn p-2 px-3 " aria-current="page" href="#">Info</a>
+                        </li>
+                    </div>
+                    <div class="d-flex px-2 fs-5 fw-bold hov-link" role="search">
+                        <li class="nav-item navbar-nav btn-getStarted">
+                            <a class="btn p-2 px-3" aria-current="page" href="#">Get Started</a>
+                        </li>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <div class="container-fluid pt-4 py-5" style="background-color:#ffffff">
+            <div class="row align-items-md-stretch">
+                <div class="col-md-5">
+                    <div class=" p-5 mx-3" style="color:#6d46c7">
+                        <h1 class="text-left fw-bolder pt-5 pb-3 customer-service-title">Customer <br>Service</h1>
+                        <p class="fs-5 banner-info">
+                            Welcome to Unique Cable Internet
+                            Customer First, Always: Share your Thoughts about Unique Cable Services
+                        </p>
+                        <div class="btn-learnMore pt-2">
+                            <a class="btn p-2 px-5" aria-current="page" href="#">Learn More</a>
+                        </div>  
+                    </div>    
+                </div>
+                <div class="col-md-7">
+                    <div class="px-5">
+                        <img src="src/imgs/banner1.png" class="img-fluid rounded"></img>
+                    </div>  
+                </div>   
+            </div>
+        </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    </body>
+</html>
